b4f192e7a11071e47b93cf69a1b6ab098c3ab415
Ngô Văn Quân <25648131.quan@student.iuh.edu.vn>
2026-01-11 23:14:01 +0700

    change

--- mains.js
+++ mains.js
@@ -1,10 +1,13 @@
 const menu = document.getElementById("menu");
 const remove = document.getElementById("remove");
 const container = document.querySelector(".container");
+const boxCircle = document.querySelector(".boxCircle");
 menu.addEventListener("click", () => {
     container.classList.add("show-nav");
+    boxCircle.classList.add("boxCircleChange");
 });
 
 remove.addEventListener("click", () => {
     container.classList.remove("show-nav");
+    boxCircle.classList.remove("boxCircleChange");
 });
